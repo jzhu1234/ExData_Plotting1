7ce70c5a97e2bdeb0a8c4c87b753ce891eafc787
John Zhu <zhujohn64@gmail.com>
2016-07-10 15:36:30 -0400

    Updated plot1.R and added plot1.png

--- plot1.R
+++ plot1.R
@@ -3,10 +3,13 @@
 library(lubridate)
 
 ## Read in Data Table and convert Date and Time columns into Date and Time objects
-data <- read.table(file = "household_power_consumption.txt",sep = ";",header = TRUE,na.strings = "?")
+data <- read.table(file = "household_power_consumption.txt",sep = ";",header = TRUE,na.strings = "?", 
+                   colClasses =c("factor","factor","numeric","numeric","numeric","numeric","numeric","numeric","numeric"))
 data$Date <- dmy(data$Date)
 data$Time <- hms(data$Time)
 data <- subset(data, Date >= ymd("2007-02-01") & Date <= ymd("2007-02-02"))
 
 ## Construct first plot, which is a histogram of global active power
-hist(data$Global_active_power,col = "red",main = "Global Active Power",xlab = "Global Active Power (killowats)")
+hist(data$Global_active_power,col = "red",main = "Global Active Power",xlab = "Global Active Power (kilowatts)")
+dev.copy(png,file = "plot1.png")
+dev.off()
